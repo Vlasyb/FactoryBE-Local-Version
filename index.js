249fc7fed035ef2522ee8094e49bbb05991b2df3
Vlasyb <vlasyb@gmail.com>
2023-06-18 20:19:05 +0300

    removed asdkas

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors({ origin: [`http://localhost:3000`], credentials: true }))
 app.use(express.json())
 app.use(cookieparser())
 
-// Routers ------------------- ++++++++ ]]]]]skdhalksdhasdhalksdhlasdhalsdh
+// Routers ------------------- ++++++++
 app.use("/employees", employeesRouter)
 app.use("/departments", departmentsRouter)
 app.use("/shifts", shiftsRouter)
